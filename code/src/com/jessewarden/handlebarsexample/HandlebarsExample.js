59b4c6e1a1049ce203634cb8d8ec70ecd3de9273
Jesse Warden <github@jessewarden.com>
2012-11-24 20:59:51 -0500

    correct require example
    Got Handlebars via HBS compiling and used in the correct require js way.

--- code/src/com/jessewarden/handlebarsexample/HandlebarsExample.js
+++ code/src/com/jessewarden/handlebarsexample/HandlebarsExample.js
@@ -1,34 +1,14 @@
-define(["com/jessewarden/handlebarsexample/utils/HandlebarsTemplateLoader.js",
+define(["hbs!com/jessewarden/handlebarsexample/templates/SimpleTemplate.hbs",
 		"jquery"], 
-		function(HandlebarsTemplateLoader, $)
+		function(SimpleTemplate, $)
 {
-	//logger.debug("*** HandlebarsExample ***")
-	//logger.debug("Handlebars: ", Handlebars);
-	//logger.debug("HandlebarsTemplateLoader: ", HandlebarsTemplateLoader);
-	//logger.debug("$: ", $);
 
-	var tpl = new HandlebarsTemplateLoader();
-	var basicTemplateFilePath = "com/jessewarden/handlebarsexample/templates/SimpleTemplate.hbs";
-	tpl.loadTemplates([basicTemplateFilePath],
-		function()
-		{
-			logger.debug("HandlebarsExample::loadTemplates completed.");
-			// grab the text
-			var simpleTemplateHTML = tpl.get(basicTemplateFilePath);
-			logger.debug("simpleTemplateHTML: " + simpleTemplateHTML);
-			// compile it Handlebars style
-			var simpleTemplateCompiled = Handlebars.compile(simpleTemplateHTML);
-			logger.debug("simpleTemplateCompiled: " + simpleTemplateCompiled);
-			// give it some some data to munch on
-			var data = {firstName: "Jesse Warden", description: "Uber crackead ready to rock the mic."};
-			var templateHTML = simpleTemplateCompiled(data);
-			logger.debug("templateHTML: ", templateHTML);
-			// inject it!
-			logger.debug("targetDiv: ", $('#content'));
-			logger.debug("before: ", $('#content').html());
-			$('#content').html(templateHTML);
-			logger.debug("after: ", $('#content').html());
+	logger.debug("simpleTemplateHTML: ", simpleTemplateHTML);
 
-		});
+	// give it some some data to munch on
+	//var data = {firstName: "Jesse Warden", description: "Uber crackead ready to rock the mic."};
+	//var templateHTML = SimpleTemplate(data);
+	// inject it!
+	//$('#content').html(templateHTML);
 
 });
